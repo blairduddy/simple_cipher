7fd00f4d9ccad9ef7482be39558a5b4b7ff1182d
Blair Duddy <64046027+blairduddy@users.noreply.github.com>
2023-10-12 21:09:31 -0400

    Add files via upload

---
+++ simple_cipher.py
@@ -0,0 +1,19 @@
+
+
+def encrypt_message(plaintext, n):
+    encrypted_message = ""
+
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+
+        if char == " ":
+            encrypted_message += " "
+        elif (char.isupper()):
+            encrypted_message += chr((ord(char) + n-65) % 26+65)
+        else:
+            encrypted_message += chr((ord(char) + n-97) % 26+97)
+
+    return encrypted_message
+
+
+print (encrypt_message('Hello, World', 7))
